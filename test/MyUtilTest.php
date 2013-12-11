130ee360dfc3fbe3c1d385531ac3988b7b1052f3
tanakayuki <a0673040@gmail.com>
2013-12-11 14:53:05 +0900

    失敗するテストをコミットしてみる

--- test/MyUtilTest.php
+++ test/MyUtilTest.php
@@ -5,4 +5,7 @@ class MyUtilTest extends PHPUnit_Framework_TestCase {
     public function testAdd() {
         $this->assertEquals(3, MyUtil::add(1, 2));
     }
+    public function testAdd_2() {
+        $this->assertEquals(5, MyUtil::add(2, 2));
+    }
 }
